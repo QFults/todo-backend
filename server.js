7922469bd1fdaa6f36741e30d2e905f61f24d24e
Quinton Fults <quinton.fults.work@gmail.com>
2022-12-09 11:39:45 -0800

    code

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const fs = require('fs')
+const { join } = require('path')
 
 const app = express()
 
@@ -7,9 +9,24 @@ app.use(express.json())
 
 app.use(express.static('public'))
 
+app.get('/api/items', (req, res) => {
+  fs.readFile(join(__dirname, 'db', 'items.json'), 'utf8', (err, data) => {
+    if (err) throw err
+    res.json(JSON.parse(data))
+  })
+})
+
 app.post('/api/items', (req, res) => {
-  console.log(req.body)
-  res.sendStatus(200)
+  fs.readFile(join(__dirname, "db", "items.json"), "utf8", (err, data) => {
+    if (err) throw err;
+    const items = JSON.parse(data)
+    items.push(req.body)
+
+    fs.writeFile(join(__dirname, "db", "items.json"), JSON.stringify(items), err1 => {
+      if (err1) throw err1
+      res.sendStatus(200)
+    });
+  });
 })
 
 app.listen(3001)
